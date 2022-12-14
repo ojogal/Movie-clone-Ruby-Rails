c654c262428921f59ef00d114339b54be735219d
ojogal <ojog.al@gmail.com>
2022-12-14 20:21:11 +0200

    validation added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,10 @@
 class Movie < ApplicationRecord
+  validates :rating, numericality: { only_integer: true }, allow_blank: true
+  validates :rating, inclusion: 1..10, allow_blank: true
+  validates :image, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :text, presence: true, uniqueness: true
+  validates :category, presence: true
   belongs_to :user
 
   has_attached_file :image, styles: { medium: "400x600#" }

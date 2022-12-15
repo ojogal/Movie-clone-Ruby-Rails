3dabd336bfb4725f5e69e1c4e85ae47df2d18aec
ojogal <ojog.al@gmail.com>
2022-12-15 20:19:53 +0200

    Admin features added

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,10 @@
 module Admin
-  class UsersController < BaseController
+  class UsersController < ApplicationController
+    before_action :authenticate_admin!
+    before_action :set_user, only: %i[ edit update destroy ]
+
     # before_action :require_authentication
     # before_action :set_user!, only: %i[edit update destroy]
-    # before_action :authenticate_user!
     # after_action :verify_authorized
 
     def index
@@ -10,45 +12,67 @@ module Admin
         format.html do
           @users = User.order(created_at: :desc)
         end
-    end
-
-    def create
-
-      redirect_to admin_users_path
-    end
-
-    def edit; end
-
-    def update
-      if @user.update user_params
-        flash[:success] = t '.success'
-        redirect_to admin_users_path
-      else
-        render :edit
       end
     end
 
+    def create
+      def create
+        @user = User.new(user_params)
+    
+        respond_to do |format|
+          if @user.save
+            format.html { redirect_to admin_users_path(@user), notice: "User was successfully created." }
+          else
+            format.html { render :index, status: :unprocessable_entity }
+          end
+        end
+      end
+    end
+
+    def edit
+    end
+
+    # def update
+    #   if @user.update user_params
+    #     flash[:success] = t '.success'
+    #     redirect_to admin_users_path
+    #   else
+    #     render :edit
+    #   end
+    # end
+      def update
+        respond_to do |format|
+          if @user.update(user_params)
+            format.html { redirect_to admin_users_path, notice: "User was successfully updated." }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+          end
+        end
+      end
+
     def destroy
       @user.destroy
-      flash[:success] = t '.success'
+      flash[:success] = 'success'
       redirect_to admin_users_path
     end
 
-    # private
+    private
+    def authenticate_admin!
+      authenticate_user!
+      redirect_to movies_path, status: :forbidden unless current_user.admin_role?
+    end    
 
-    # def set_user!
-    #   @user = User.find params[:id]
-    # end
+    def set_user
+      @user = User.find params[:id]
+    end
 
-    # def user_params
-    #   params.require(:user).permit(
-    #     :email, :name, :password, :password_confirmation, :role
-    #   ).merge(admin_edit: true)
-    # end
+    def user_params
+      params.require(:user).permit(:email, :role)#.merge(admin_edit: true).merge(admin_update: true)
+    end
 
     # def authorize_user!
     #   authorize(@user || User)
     # end
+  
   end
-end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :movies do
     get '/page/:page', action: :index, on: :collection
   end
   
   namespace :admin do
-    resources :users, only: %i[index show create edit update]
+    resources :users
+    root "users#index"
   end
 
   root "movies#index"

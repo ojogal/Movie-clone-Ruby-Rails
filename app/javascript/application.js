355b48ac675fbeb3c489b54eafcf5fb4bae030e0
ojogal <ojog.al@gmail.com>
2022-12-26 15:26:36 +0200

    application.js changed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,5 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 // import './add_jquery'
-require("jquery.raty")
-
-
-
+// require("jquery.raty")
+import "app/assets/builds/jquery.raty.js"

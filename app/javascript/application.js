10e2ac06c1dd4d8d300f7464ba0cc6dcb68f0e31
ojogal <ojog.al@gmail.com>
2022-12-26 20:09:58 +0200

    javascript updated

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,6 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 // import './add_jquery'
-require("jquery.raty")
-
+import "../assets/builds/jquery.raty.js"
 
 
